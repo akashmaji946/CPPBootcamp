0126d79ece57410881bdea5dea4cdd06321c2bff
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-06-23 21:04:23 +0530

    Day 02 folder

---
+++ Daywise Code Files/Day-02-CPP/02_03_names.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	// names
+
+	int no_of_oranges = 10;   // underscore
+	string myName = "Akash";
+	
+	// 90000.0;
+	// "900000"
+
+	//myname  myName
+	// myHouseAddress
+	// yourFavouriteBook    => camel casing
+
+
+	return 0;
+}
