8a480c8467d24e1d27134bb3b23a7135e66bf809
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-06-23 21:03:40 +0530

    Day 1 folder

---
+++ Daywise Code Files/Day-01-CPP/01_01_main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// library => functions + variables
+
+// object => cout
+using namespace std;
+// basic input output => cin     cout
+
+// main is the starting point
+// int => integer    0, 1, 2, ..........(integer)
+int main() {
+
+
+  // standard namespace
+  //   \n    => new line
+  //   endl   => end line
+  cout << "Hello CPP Buddies" << endl;   
+  return 0;
+  // OS 
+}
