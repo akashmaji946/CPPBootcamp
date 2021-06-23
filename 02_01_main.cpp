afd41e65824e80c22293b1a9c29964d2806af4c0
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-06-23 21:02:14 +0530

    Day 02 files

---
+++ 02_01_main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "this is day 2\n";
+
+	// int => 2, 4, 6, 9, -12, ....
+
+	int age = 50;
+	cout << age + 10 << "\n";
+
+	// double => 123.98
+	double percent = 87.98;  // bty default
+	cout << percent << endl;
+
+	// float
+	float salary = 129.8F;    // F  => float
+	cout << salary << endl;
+
+	// char  =>   A    B    C    D   (letters)
+
+	char ch = 'A';   // characters => single quotes
+	ch = '+';
+	cout << ch << endl;
+
+	string name = "manoj";
+	cout << name << endl;
+
+	age = age + 10;   // 50+ 10 => 60   .... age=60
+	cout << age << endl;   // value changed
+
+
+	name = name + " singh";   // manoj + singh  => manojsingh
+	cout << name << endl;
+
+
+
+
+
+
+	return 0;
+}
---
+++ 02_02_input.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	//user input
+	long int age;  // no value   => garbage value
+
+	// input => cin  (console input)
+	cout << "Enter Your Age: ";
+	cin >> age;
+	cout << "Your age is: ";
+	cout << age << endl;
+
+	// name
+	string name;
+	cout << "Enter Your Name: ";
+	cin >> name;    /// Akash   "Akash"
+	cout << "Your name is: ";
+	cout << name << endl;
+
+
+
+
+
+	return 0;
+}
