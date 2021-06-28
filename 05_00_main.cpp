5bb2efd43a1c31dd393b63f809f944cd9f5e3489
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-06-28 20:10:50 +0530

    Day 05 code files

---
+++ 05_00_main.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // if condition
+
+    int a = 10;
+    
+
+    // ==     equality
+    if(a == 5){
+      cout << "a is 10" << endl;
+      /////
+      /////
+      /////
+    }
+
+    cout << " I am outside if\n";
+
+    return 0;
+}
